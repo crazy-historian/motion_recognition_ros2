93d6f170e1992a43ed7c009a40f02646f0e91c91
crazy_historian <zaitsev808@mail.ru>
2022-03-14 14:48:16 +0300

    implemented frames delay counter in MotionRecognizer.

--- src/build/motion_recognition/build/lib/motion_recognition/motion_recognition.py
+++ src/build/motion_recognition/build/lib/motion_recognition/motion_recognition.py
@@ -10,8 +10,23 @@ import numpy as np
 import cv2
 
 
+class Counter:
+    def __init__(self, frames: int):
+        self.frames = frames
+        self.counter = self.frames
+
+    def decrement(self) -> None:
+        self.counter -= 1
+
+    def reset(self) -> None:
+        self.counter = self.frames
+
+    def is_finished(self) -> bool:
+        return self.counter == 0
+
+
 class MotionRecognizer(Node):
-    def __init__(self):
+    def __init__(self, frames_delay: int = 5):
         super().__init__("convert_node")
         self.get_logger().info("Starting work")
         self.subscriber = self.create_subscription(Image, "/image_raw", self.listener_callback, 10)
@@ -20,33 +35,39 @@ class MotionRecognizer(Node):
         self.previous_frame = None
         self.current_frame = None
         self.kernel = np.ones((5, 5))
+        self.counter = Counter(frames_delay)
+        self.last_result = 0
 
     def _check_motion(self, img):
-        # TODO: implement algorithm here
         if self.previous_frame is None:
             self.previous_frame = img
-            self.current_frame = img
-            return 0
-        self.previous_frame = self.current_frame
-        self.current_frame = img
-
-        self.previous_frame = self.previous_frame
-        self.current_frame = self.current_frame
-
-        diff_frame = cv2.absdiff(self.previous_frame, self.current_frame)
-        diff_gray = cv2.cvtColor(diff_frame, cv2.COLOR_BGR2GRAY)
-        diff_blur = cv2.GaussianBlur(diff_gray, (5, 5), 0)
-        diff_dilated = cv2.dilate(diff_blur, self.kernel, 1)
-        thresh_frame = cv2.threshold(diff_dilated, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
-
-        contours, hierarchy = cv2.findContours(thresh_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        if len(contours) > 0:
-            self.get_logger().info('1')
-            return 1
-        else:
             self.get_logger().info('0')
             return 0
+
+        if self.counter.is_finished():
+            # diff_frame = cv2.absdiff(self.previous_frame, self.current_frame)
+            diff_frame = cv2.absdiff(self.current_frame, self.previous_frame)
+            diff_gray = cv2.cvtColor(diff_frame, cv2.COLOR_BGR2GRAY)
+            diff_blur = cv2.GaussianBlur(diff_gray, (5, 5), 0)
+            diff_dilated = cv2.dilate(diff_blur, self.kernel, 1)
+            thresh_frame = cv2.threshold(diff_dilated, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+
+            contours, hierarchy = cv2.findContours(thresh_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            if len(contours) > 0:
+                self.last_result = 1
+                self.get_logger().info('1')
+            else:
+                self.last_result = 0
+                self.get_logger().info('0')
+
+            self.previous_frame = self.current_frame
+            self.counter.reset()
+        else:
+            self.counter.decrement()
+            self.current_frame = img
+
+        return self.last_result
 
     def listener_callback(self, msg: Image):
         img_from_cam = self.bridge.imgmsg_to_cv2(msg, "bgr8")
--- src/motion_recognition/motion_recognition/motion_recognition.py
+++ src/motion_recognition/motion_recognition/motion_recognition.py
@@ -10,8 +10,23 @@ import numpy as np
 import cv2
 
 
+class Counter:
+    def __init__(self, frames: int):
+        self.frames = frames
+        self.counter = self.frames
+
+    def decrement(self) -> None:
+        self.counter -= 1
+
+    def reset(self) -> None:
+        self.counter = self.frames
+
+    def is_finished(self) -> bool:
+        return self.counter == 0
+
+
 class MotionRecognizer(Node):
-    def __init__(self):
+    def __init__(self, frames_delay: int = 5):
         super().__init__("convert_node")
         self.get_logger().info("Starting work")
         self.subscriber = self.create_subscription(Image, "/image_raw", self.listener_callback, 10)
@@ -20,40 +35,51 @@ class MotionRecognizer(Node):
         self.previous_frame = None
         self.current_frame = None
         self.kernel = np.ones((5, 5))
+        self.counter = Counter(frames_delay)
+        self.last_result = 0
 
     def _check_motion(self, img):
-        # TODO: implement algorithm here
         if self.previous_frame is None:
             self.previous_frame = img
-            self.current_frame = img
-            return 0
-        self.previous_frame = self.current_frame
-        self.current_frame = img
-
-        self.previous_frame = self.previous_frame
-        self.current_frame = self.current_frame
-
-        diff_frame = cv2.absdiff(self.previous_frame, self.current_frame)
-        diff_gray = cv2.cvtColor(diff_frame, cv2.COLOR_BGR2GRAY)
-        diff_blur = cv2.GaussianBlur(diff_gray, (5, 5), 0)
-        diff_dilated = cv2.dilate(diff_blur, self.kernel, 1)
-        thresh_frame = cv2.threshold(diff_dilated, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
-
-        contours, hierarchy = cv2.findContours(thresh_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        if len(contours) > 0:
-            self.get_logger().info('1')
-            return 1
-        else:
             self.get_logger().info('0')
             return 0
+
+        if self.counter.is_finished():
+            diff_frame = cv2.absdiff(self.current_frame, self.previous_frame)
+            diff_gray = cv2.cvtColor(diff_frame, cv2.COLOR_BGR2GRAY)
+            diff_blur = cv2.GaussianBlur(diff_gray, (5, 5), 0)
+            diff_dilated = cv2.dilate(diff_blur, self.kernel, 1)
+            thresh_frame = cv2.threshold(diff_dilated, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+
+            contours, hierarchy = cv2.findContours(thresh_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            if len(contours) > 0:
+                self.last_result = 1
+                self.get_logger().info('1')
+            else:
+                self.last_result = 0
+                self.get_logger().info('0')
+
+            for contour in contours:
+                x, y, w, h = cv2.boundingRect(contour)
+                if cv2.contourArea(contour) > 300:
+                    cv2.rectangle(self.current_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            self.publisher_2.publish()
+
+            self.previous_frame = self.current_frame
+            self.counter.reset()
+        else:
+            self.counter.decrement()
+            self.current_frame = img
+
+        return self.last_result
 
     def listener_callback(self, msg: Image):
         img_from_cam = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         motion_flag = self._check_motion(img_from_cam)
         msg = Int8()
         msg.data = int(motion_flag)
-        self.publisher.publish(msg)
 
 
 def main(args=None):
